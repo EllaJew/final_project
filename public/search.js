3b6ae9f2d8c4d5331e53a32c4d73970356afe681
EllaJew <ejew2@terpmail.umd.edu>
2026-05-12 16:50:47 -0400

    adding bar chart

--- public/search.js
+++ public/search.js
@@ -1,3 +1,5 @@
+let barChart;
+
 async function searchFruit() {
     const fruit = document.getElementById("fruitInput").value;
 
@@ -7,12 +9,37 @@ async function searchFruit() {
     console.log("Data:", data);
 
     document.getElementById("temporaryOutput").innerHTML =
-    `<h3>${data.name}</h3>
-    <p>Carbs: ${data.carbs}</p>
-    <p>Protein: ${data.protein}</p>
-    <p>Fat: ${data.fat}</p>
-    <p>Calories: ${data.calories}</p>
-    <p>Sugar: ${data.sugar}</p>`;
+        `<h3>${data.name}</h3>
+        <p>Carbs: ${data.carbs}</p>
+        <p>Protein: ${data.protein}</p>
+        <p>Fat: ${data.fat}</p>
+        <p>Calories: ${data.calories}</p>
+        <p>Sugar: ${data.sugar}</p>`;
+}
+
+function nutritionChart(data) {
+    document.getElementById("temporaryOutput").innerHTML =
+        `<h3>${data.name}</h3>
+        <p>Carbs: ${data.carbs}</p>
+        <p>Protein: ${data.protein}</p>
+        <p>Fat: ${data.fat}</p>
+        <p>Calories: ${data.calories}</p>
+        <p>Sugar: ${data.sugar}</p>`;
+    
+        const chart = document.getElementById("barChart");
+        
+        if (barChart) {
+            barChart.destroy();}
+
+        barChart = new Chart(chart, {
+            type: "bar",
+            data: {labels: ["Carbs", "Protein", "Fat", "Calories", "Sugar"],
+            datasets: [{
+                label: `${data.name} Nutrition`,
+                data: [data.carbs, data.protein, data.fat, data.calories,
+                       data.sugar]}]
+        },
+    });
 }
 
 window.onload = () => {
