18dc76fa59e237161d45b8d34001c5f6a6d54982
EllaJew <ejew2@terpmail.umd.edu>
2026-05-11 22:08:39 -0400

    edit root route

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ const supabaseURL = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = supabaseClient.createClient(supabaseURL, supabaseKey);
 
+const path = require("path");
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "search_page.html"));
+});
+
 //GET from Supabase
 app.get('/recipes', async (req, res) => {
     console.log("Getting all recipes");
