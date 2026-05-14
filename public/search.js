e841c924daa3c9a6326f172d95940cf11fbf2970
EllaJew <ejew2@terpmail.umd.edu>
2026-05-13 21:03:00 -0400

    changing styles

--- public/search.js
+++ public/search.js
@@ -127,6 +127,8 @@ const fruit_list = ["apple", "lime", "lemon", "cherry", "banana", "orange", "pin
 async function getFruitImage(fruit) {
     const res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${fruit}`);
     const data = await res.json();
+
+    return data.meals?.[0]?.strMealThumb;
 }
 
 async function createSlider() {
